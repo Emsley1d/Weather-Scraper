3e922293c1299a20dd6098160367dbdc81c5a461
emsleyd <emsleyd@gmail.com>
2023-02-10 12:29:10 +0000

    issue - browser not asking permission to access location

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,24 @@ require('dotenv').config();
 
 const apiKey = process.env.API_KEY;
 
+// onload event listener for api:
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // accesses location of user:
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const locationData = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}`;
+
+            fetch(locationData).then((response) => {
+                return response.json();
+            });
+        }
+)}
+});
+
+
+
+console.log(locationData)
--- ReadMe/readme.md
+++ ReadMe/readme.md
@@ -9,3 +9,9 @@ A location based weather app to show current the current weather data depending 
 I want to keep the design clean and simple as below:
 
 ![wireframe](/ReadMe/Wireframe.png)
+
+## Current Issues:
+
+1. Browser (Chrome) not asking for location. 
+    * Re-opened live server in Safari but Safari doesnt ask for permission to access location either.
+
