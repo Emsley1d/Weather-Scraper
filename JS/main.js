26943752c4e9ed963914ae3b1ebc90d35385095b
emsleyd <emsleyd@gmail.com>
2023-02-10 12:52:46 +0000

    fixing location issues

--- JS/main.js
+++ JS/main.js
@@ -4,20 +4,27 @@ const apiKey = process.env.API_KEY;
 
 // onload event listener for api:
 window.addEventListener('load', () => {
-    let long;
+    let lon;
     let lat;
     // accesses location of user:
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-            long = position.coords.longitude;
+        try {
+            lon = position.coords.longitude;
             lat = position.coords.latitude;
-            const locationData = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}`;
+            const locationData = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
             fetch(locationData).then((response) => {
                 return response.json();
-            });
+            })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }  catch (error) {
+            console.error(error);
         }
-)}
+    });
+};
 });
 
 
--- ReadMe/readme.md
+++ ReadMe/readme.md
@@ -14,4 +14,18 @@ I want to keep the design clean and simple as below:
 
 1. Browser (Chrome) not asking for location. 
     * Re-opened live server in Safari but Safari doesnt ask for permission to access location either.
+    * Confirmed Chrome & Safari had permission to access my location on my Mac.
+    * Realised I hadn't correctly used string interpolation for the locationData URL; so corrected URL.
+    * Code wrapped in try-catch and only error is:
+
+    ```json
+    {
+    window.addEventListener('load', () => {
+    ^
+
+    ReferenceError: window is not defined
+    }
+    ```
+
+
 
